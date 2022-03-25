6bcfc2e02fd3e41c6eeab50b7f225b98514abf34
chulps <chuckoward@gmail.com>
2022-03-25 12:01:40 +0700

    fixed dropdown styling

--- components/UI/Dropdown.js
+++ components/UI/Dropdown.js
@@ -2,7 +2,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, View } from 'react-native';
 import RNPickerSelect from 'react-native-picker-select';
-import fonts from '../styles/fonts'
+import fonts from '../styles/fonts';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
 const componentStyles = StyleSheet.create({
@@ -11,7 +12,11 @@ const componentStyles = StyleSheet.create({
         // marginBottom: 12,
         borderWidth: 1,
         padding: 16,
-        backgroundColor: 'red'
+        borderColor: '#330099', // green0
+        padding: 16,
+        backgroundColor: 'hsla(120,100%,20%, 0.1)', // greenC-10
+        display: 'flex',
+        alignItems: 'center',
     }
 });
 
@@ -35,6 +40,7 @@ class Dropdown extends Component {
                     placeholder={(placeholder) ? {label: placeholder} : {label: ''}}
                     onValueChange={(value) => onChange(value)}
                     items={options}
+                    Icon={() => <Ionicons name={'chevron-down'} size={20} />}
                     textInputProps={{
                         style: {
                             fontFamily: fonts.default,
