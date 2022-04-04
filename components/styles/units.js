6447cf11acb7dd90e0ac7c3ceda95c67c6dda22b
Charles Howard <charleshoward@Charless-MacBook-Pro.local>
2022-04-03 18:51:18 -0700

    we ridin spinnas

--- components/styles/units.js
+++ components/styles/units.js
@@ -7,14 +7,22 @@ const unit2 = unit1 * ratio.unitScaleRatio;
 const unit3 = unit2 * ratio.unitScaleRatio;
 const unit4 = unit3 * ratio.unitScaleRatio;
 const unit5 = unit4 * ratio.unitScaleRatio;
+const unit6 = unit5 * ratio.unitScaleRatio;
+const unit7 = unit6 * ratio.unitScaleRatio;
+const unit8 = unit7 * ratio.unitScaleRatio;
+const unit9 = unit8 * ratio.unitScaleRatio;
 
 const units = {
-    unit0,
-    unit1,
-    unit2,
-    unit3,
-    unit4,
-    unit5
-}
+  unit0,
+  unit1,
+  unit2,
+  unit3,
+  unit4,
+  unit5,
+  unit6,
+  unit7,
+  unit8,
+  unit9,
+};
 
 export default units;
