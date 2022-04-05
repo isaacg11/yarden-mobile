04010582c12c86391c43d7753ba9087e8d739472
Charles Howard <charleshoward@Charless-MacBook-Pro.local>
2022-04-04 17:03:40 -0700

    added collapse styling

--- components/UI/Collapse.js
+++ components/UI/Collapse.js
@@ -1,46 +1,48 @@
-
-import React, { Component } from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import React, {Component} from 'react';
+import {View, TouchableOpacity} from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import Paragraph from './Paragraph';
+import colors from '../styles/colors';
 
 class Collapse extends Component {
+  state = {};
 
-    state = {}
-
-    componentDidMount() {
-        if(this.props.open) {
-            this.setState({isOpen: true});
-        }
+  componentDidMount() {
+    if (this.props.open) {
+      this.setState({isOpen: true});
     }
+  }
 
-    render() {
+  render() {
+    const {title = 'View Details', content} = this.props;
 
-        const {
-            title = 'View Details',
-            content
-        } = this.props;
+    const {isOpen} = this.state;
 
-        const {
-            isOpen
-        } = this.state;
-
-        return (
-            <View style={{ backgroundColor: '#fff', padding: 12, borderRadius: 5 }}>
-                <TouchableOpacity onPress={() => this.setState({ isOpen: !isOpen })}>
-                    <View style={{ display: 'flex', flex: 1, alignSelf: 'stretch', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-                        <Paragraph style={{ fontWeight: 'bold'}}>{title}</Paragraph>
-                        <Ionicons name={`caret-${(isOpen) ? 'up' : 'down' }-circle-outline`} size={30} style={{ alignSelf: 'flex-end' }} />
-                    </View>
-                </TouchableOpacity>
-                {(isOpen) && (
-                    <View>
-                        {content}
-                    </View>
-                )}
-            </View>
-        )
-    }
+    return (
+      <View style={{backgroundColor: '#fff', padding: 12, borderRadius: 5}}>
+        <TouchableOpacity onPress={() => this.setState({isOpen: !isOpen})}>
+          <View
+            style={{
+              display: 'flex',
+              flex: 1,
+              alignSelf: 'stretch',
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <Paragraph style={{fontWeight: 'bold'}}>{title}</Paragraph>
+            <Ionicons
+              name={`caret-${isOpen ? 'up' : 'down'}`}
+              size={30}
+              style={{alignSelf: 'flex-end'}}
+              color={colors.purpleB}
+            />
+          </View>
+        </TouchableOpacity>
+        {isOpen && <View>{content}</View>}
+      </View>
+    );
+  }
 }
 
 module.exports = Collapse;
