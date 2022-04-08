aac4d121c661f1fe59537b6ac0f3144cc068af92
Charles Howard <charleshoward@Charless-MacBook-Pro.local>
2022-04-07 19:26:02 -0700

    updated credit card component and made adjustments to font sizes, input stylying, dropdown styling and some other stuff

--- screens/Enrollment.js
+++ screens/Enrollment.js
@@ -1,211 +1,217 @@
-
-import React, { Component } from 'react';
-import { SafeAreaView, View, ScrollView } from 'react-native';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, {Component} from 'react';
+import {SafeAreaView, View, ScrollView} from 'react-native';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import moment from 'moment';
 import Button from '../components/UI/Button';
-import { alert } from '../components/UI/SystemAlert';
+import {alert} from '../components/UI/SystemAlert';
 import LoadingIndicator from '../components/UI/LoadingIndicator';
 import Header from '../components/UI/Header';
 import Plans from '../components/app/Plans';
-import Card from '../components/app/Card';
-import { getPlans } from '../actions/plans/index';
-import { updateUser } from '../actions/user/index';
-import { createSubscription } from '../actions/subscriptions/index';
-import { createOrder, getOrders } from '../actions/orders/index';
+import CreditCard from '../components/app/CreditCard';
+import {getPlans} from '../actions/plans/index';
+import {updateUser} from '../actions/user/index';
+import {createSubscription} from '../actions/subscriptions/index';
+import {createOrder, getOrders} from '../actions/orders/index';
 import getOrderDescription from '../helpers/getOrderDescription';
 import units from '../components/styles/units';
 
 class Enrollment extends Component {
+  state = {};
 
-    state = {}
+  async componentDidMount() {
+    // show loading indicator
+    this.setState({isLoading: true});
 
-    async componentDidMount() {
-        // show loading indicator
-        this.setState({ isLoading: true });
+    // get all plans
+    await this.props.getPlans();
 
-        // get all plans
-        await this.props.getPlans();
+    // hide loading indicator
+    this.setState({isLoading: false});
+  }
 
-        // hide loading indicator
-        this.setState({ isLoading: false });
+  async next() {
+    // if checkout work-flow {...}
+    if (this.props.route.params.isCheckout) {
+      // determine if a plan was selected
+      const plan =
+        this.state.selectedPlan !== 'none'
+          ? this.state.selectedPlan._id
+          : 'none';
+
+      // set quote and plants data
+      const quoteAndPlants = this.props.route.params;
+
+      // combine quote, plants, and plan
+      const quoteAndPlantsAndPlan = {
+        ...quoteAndPlants,
+        ...{plan: plan},
+      };
+
+      // navigate to checkout
+      this.props.navigation.navigate('Checkout', quoteAndPlantsAndPlan);
+    } else {
+      // if no garden info {...}
+      if (!this.props.user.garden_info) {
+        // show warning
+        return alert(
+          'We cannot find a garden associated with your account. You must have a garden installed to start your subscription.',
+          'No Garden Found',
+        );
+      } else if (!this.props.user.payment_info) {
+        // if no payment info {...}
+
+        // show the credit card modal
+        this.setState({isOpen: true});
+
+        // show warning
+        return alert('Please add a payment method', 'No Payment Method Found');
+      } else {
+        alert(
+          `Upon clicking the OK button, you will be enrolled in Yarden's "${
+            this.state.selectedPlan.type
+          }" for routine garden maintenance. A total payment of $${this.state.selectedPlan.rate.toFixed(
+            2,
+          )} will be charged today ${moment().format(
+            'MM/DD/YYYY',
+          )} using the credit card on file ending in ${
+            this.props.user.payment_info.card_last4
+          }`,
+          'Start Subscription?',
+          async () => {
+            // show loading indicator
+            this.setState({isLoading: true});
+
+            // enroll customer in subscription plan
+            await this.startSubscription();
+
+            // navigate to subscription
+            this.props.navigation.navigate('Subscription');
+
+            // show loading indicator
+            this.setState({isLoading: false});
+          },
+          true,
+        );
+      }
     }
+  }
 
-    async next() {
+  async startSubscription() {
+    // format subscription
+    const newSubscription = {
+      customerId: this.props.user.payment_info.customer_id,
+      userId: this.props.user._id,
+      selectedPlan: this.state.selectedPlan.type,
+      trialPeriod: 'none',
+    };
 
-        // if checkout work-flow {...}
-        if (this.props.route.params.isCheckout) {
+    // create new subscription
+    const subscription = await this.props.createSubscription(newSubscription);
 
-            // determine if a plan was selected
-            const plan = (this.state.selectedPlan !== 'none') ? this.state.selectedPlan._id : 'none';
+    // format new user info
+    const paymentInfo = this.props.user.payment_info;
+    const gardenInfo = this.props.user.garden_info;
+    paymentInfo.plan_id = subscription.id;
+    gardenInfo.maintenance_plan = this.state.selectedPlan._id;
 
-            // set quote and plants data
-            const quoteAndPlants = this.props.route.params;
+    // update user with new payment info and garden info
+    await this.props.updateUser(`userId=${this.props.user._id}`, {
+      paymentInfo: paymentInfo,
+      gardenInfo: gardenInfo,
+    });
 
-            // combine quote, plants, and plan
-            const quoteAndPlantsAndPlan = {
-                ...quoteAndPlants,
-                ...{ plan: plan }
-            }
+    // format order data
+    const workOrder = {
+      type: this.state.selectedPlan.type,
+      customer: this.props.user._id,
+      description: getOrderDescription(this.state.selectedPlan.type),
+      date: new Date(moment().add(1, 'week').startOf('day')),
+    };
 
-            // navigate to checkout
-            this.props.navigation.navigate('Checkout', quoteAndPlantsAndPlan);
-        } else {
+    // create new order
+    await this.props.createOrder(workOrder);
 
-            // if no garden info {...}
-            if (!this.props.user.garden_info) {
+    // get pending orders
+    await this.props.getOrders(
+      `status=pending&start=none&end=${new Date(moment().add(1, 'year'))}`,
+    );
+  }
 
-                // show warning
-                return alert(
-                    'We cannot find a garden associated with your account. You must have a garden installed to start your subscription.',
-                    'No Garden Found',
-                );
-            } else if (!this.props.user.payment_info) { // if no payment info {...}
+  render() {
+    const {isLoading, selectedPlan, isOpen} = this.state;
 
-                // show the credit card modal
-                this.setState({ isOpen: true });
+    const {plans, user} = this.props;
 
-                // show warning
-                return alert(
-                    'Please add a payment method',
-                    'No Payment Method Found'
-                );
-            } else {
-                alert(
-                    `Upon clicking the OK button, you will be enrolled in Yarden's "${this.state.selectedPlan.type}" for routine garden maintenance. A total payment of $${this.state.selectedPlan.rate.toFixed(2)} will be charged today ${moment().format('MM/DD/YYYY')} using the credit card on file ending in ${this.props.user.payment_info.card_last4}`,
-                    'Start Subscription?',
-                    async () => {
-                        // show loading indicator
-                        this.setState({ isLoading: true });
+    const {isCheckout} = this.props.route.params;
 
-                        // enroll customer in subscription plan
-                        await this.startSubscription();
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          width: '100%',
+        }}>
+        {/* loading indicator */}
+        <LoadingIndicator loading={isLoading} />
 
-                        // navigate to subscription
-                        this.props.navigation.navigate('Subscription');
+        {/* customer card */}
+        <CreditCard
+          newCard={!user.payment_info}
+          isOpen={isOpen}
+          close={() => this.setState({isOpen: false})}
+        />
 
-                        // show loading indicator
-                        this.setState({ isLoading: false });
-                    },
-                    true
-                );
-            }
-        }
-    }
+        <ScrollView>
+          <Header
+            type="h4"
+            style={{textAlign: 'center', marginTop: units.unit6}}>
+            Maintenance Plans
+          </Header>
+          <View style={{padding: units.unit5}}>
+            {/* plan list */}
+            <Plans
+              plans={plans}
+              onSelect={plan => this.setState({selectedPlan: plan})}
+              isCheckout={isCheckout}
+            />
 
-    async startSubscription() {
-
-        // format subscription
-        const newSubscription = {
-            customerId: this.props.user.payment_info.customer_id,
-            userId: this.props.user._id,
-            selectedPlan: this.state.selectedPlan.type,
-            trialPeriod: 'none'
-        }
-
-        // create new subscription
-        const subscription = await this.props.createSubscription(newSubscription);
-
-        // format new user info
-        const paymentInfo = this.props.user.payment_info;
-        const gardenInfo = this.props.user.garden_info;
-        paymentInfo.plan_id = subscription.id;
-        gardenInfo.maintenance_plan = this.state.selectedPlan._id;
-
-        // update user with new payment info and garden info
-        await this.props.updateUser(`userId=${this.props.user._id}`, { paymentInfo: paymentInfo, gardenInfo: gardenInfo });
-
-        // format order data
-        const workOrder = {
-            type: this.state.selectedPlan.type,
-            customer: this.props.user._id,
-            description: getOrderDescription(this.state.selectedPlan.type),
-            date: new Date(moment().add(1, 'week').startOf('day'))
-        }
-
-        // create new order
-        await this.props.createOrder(workOrder);
-
-        // get pending orders
-        await this.props.getOrders(`status=pending&start=none&end=${new Date(moment().add(1, 'year'))}`);
-    }
-
-    render() {
-
-        const {
-            isLoading,
-            selectedPlan,
-            isOpen
-        } = this.state;
-
-        const {
-            plans,
-            user
-        } = this.props;
-
-        const {
-            isCheckout
-        } = this.props.route.params;
-
-        return (
-            <SafeAreaView style={{
-                flex: 1,
-                width: "100%",
-            }}>
-                {/* loading indicator */}
-                <LoadingIndicator loading={isLoading} />
-
-                {/* customer card */}
-                <Card
-                    newCard={!user.payment_info}
-                    isOpen={isOpen}
-                    close={() => this.setState({ isOpen: false })}
-                />
-
-                <ScrollView>
-                    <Header type="h4" style={{ textAlign: 'center', marginTop: units.unit6 }}>Maintenance Plans</Header>
-                    <View style={{ padding: units.unit5 }}>
-
-                        {/* plan list */}
-                        <Plans
-                            plans={plans}
-                            onSelect={(plan) => this.setState({ selectedPlan: plan })}
-                            isCheckout={isCheckout}
-                        />
-
-                        {/* navigation button */}
-                        <View>
-                            <Button
-                                disabled={!selectedPlan || (selectedPlan !== 'none' && !selectedPlan.type)}
-                                text="Continue"
-                                onPress={() => this.next()}
-                                variant="primary"
-                            />
-                        </View>
-
-                    </View>
-                </ScrollView>
-            </SafeAreaView>
-        )
-    }
+            {/* navigation button */}
+            <View>
+              <Button
+                disabled={
+                  !selectedPlan ||
+                  (selectedPlan !== 'none' && !selectedPlan.type)
+                }
+                text="Continue"
+                onPress={() => this.next()}
+                variant="primary"
+              />
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        plans: state.plans,
-        user: state.user
-    }
+  return {
+    plans: state.plans,
+    user: state.user,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-        getPlans,
-        updateUser,
-        createOrder,
-        createSubscription,
-        getOrders,
-    }, dispatch)
+  return bindActionCreators(
+    {
+      getPlans,
+      updateUser,
+      createOrder,
+      createSubscription,
+      getOrders,
+    },
+    dispatch,
+  );
 }
 
 Enrollment = connect(mapStateToProps, mapDispatchToProps)(Enrollment);
--- screens/Product.js
+++ screens/Product.js
@@ -1,16 +1,15 @@
-
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { SafeAreaView, ImageBackground, View } from 'react-native';
+import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {SafeAreaView, ImageBackground, View} from 'react-native';
 import Input from '../components/UI/Input';
 import Button from '../components/UI/Button';
 import LoadingIndicator from '../components/UI/LoadingIndicator';
 import Paragraph from '../components/UI/Paragraph';
 import AddToCart from '../components/app/AddToCart';
-import { getItems } from '../actions/items/index';
-import { getRules } from '../actions/rules/index';
-import { getIrrigation } from '../actions/irrigation/index';
+import {getItems} from '../actions/items/index';
+import {getRules} from '../actions/rules/index';
+import {getIrrigation} from '../actions/irrigation/index';
 import capitalize from '../helpers/capitalize';
 import updateCart from '../helpers/updateCart';
 import applyProductRules from '../helpers/applyProductRules';
@@ -21,199 +20,210 @@ import delimit from '../helpers/delimit';
 import units from '../components/styles/units';
 
 class Product extends Component {
+  state = {
+    qty: '1',
+    price: 0,
+  };
 
-    state = {
-        qty: '1',
-        price: 0
+  async componentDidMount() {
+    // show loading indicator
+    this.setState({isLoading: true});
+
+    // set selected product
+    let selectedProduct = this.props.route.params.product;
+    let selectedVariant = this.props.route.params.variant;
+
+    // get product rules
+    const productRules = await this.props.getRules(
+      `product=${selectedProduct._id}`,
+      true,
+    );
+
+    // get irrigation items
+    await this.props.getIrrigation();
+
+    // set initial product materials
+    let productMaterials = selectedVariant.line_items.materials;
+
+    // if product rules exist for product {...}
+    if (productRules.length > 0) {
+      // set product measurements
+      const productMeasurements = getProductMeasurements(
+        selectedVariant.dimensions,
+        this.state.qty,
+      );
+
+      // apply rules to materials, which will calculate the qty of each item
+      productMaterials = await applyProductRules(
+        selectedVariant.line_items.materials,
+        productRules,
+        productMeasurements.sqft,
+        productMeasurements.cf,
+        productMeasurements.vf,
+        productMeasurements.lf,
+      );
+    } else {
+      // assign qty to material items based on the product qty
+      productMaterials.forEach(i => {
+        i.qty = parseInt(this.state.qty);
+      });
     }
 
-    async componentDidMount() {
+    // if product is for garden beds {...}
+    if (selectedProduct.name === 'garden bed') {
+      let materialList = [];
 
-        // show loading indicator
-        this.setState({ isLoading: true });
+      // remove irrigation items from materials
+      productMaterials.forEach(material => {
+        const isIrrigationItem = this.props.irrigation.find(
+          irr => irr.item._id === material._id,
+        );
+        if (!isIrrigationItem) materialList.push(material);
+      });
 
-        // set selected product
-        let selectedProduct = this.props.route.params.product;
-        let selectedVariant = this.props.route.params.variant;
-
-        // get product rules
-        const productRules = await this.props.getRules(`product=${selectedProduct._id}`, true);
-
-        // get irrigation items
-        await this.props.getIrrigation();
-
-        // set initial product materials
-        let productMaterials = selectedVariant.line_items.materials;
-
-        // if product rules exist for product {...}
-        if (productRules.length > 0) {
-
-            // set product measurements
-            const productMeasurements = getProductMeasurements(selectedVariant.dimensions, this.state.qty);
-
-            // apply rules to materials, which will calculate the qty of each item
-            productMaterials = await applyProductRules(
-                selectedVariant.line_items.materials,
-                productRules,
-                productMeasurements.sqft,
-                productMeasurements.cf,
-                productMeasurements.vf,
-                productMeasurements.lf
-            );
-
-        } else {
-            // assign qty to material items based on the product qty
-            productMaterials.forEach((i) => {
-                i.qty = parseInt(this.state.qty);
-            })
-        }
-
-        // if product is for garden beds {...}
-        if (selectedProduct.name === 'garden bed') {
-            let materialList = [];
-
-            // remove irrigation items from materials
-            productMaterials.forEach((material) => {
-                const isIrrigationItem = this.props.irrigation.find((irr) => irr.item._id === material._id);
-                if (!isIrrigationItem) materialList.push(material);
-            })
-
-            // set product materials
-            productMaterials = materialList;
-        }
-
-        // set materials cost
-        const materialsCost = calculateMaterials(productMaterials);
-
-        // set labor cost
-        const laborCost = calculateLabor(selectedVariant.line_items.labor);
-
-        // update UI
-        this.setState({
-            price: (materialsCost + laborCost),
-            isLoading: false
-        })
-
+      // set product materials
+      productMaterials = materialList;
     }
 
-    async addToCart() {
+    // set materials cost
+    const materialsCost = calculateMaterials(productMaterials);
 
-        // set product
-        const product = this.props.route.params.product;
+    // set labor cost
+    const laborCost = calculateLabor(selectedVariant.line_items.labor);
 
-        // set variant
-        const variant = this.props.route.params.variant;
+    // update UI
+    this.setState({
+      price: materialsCost + laborCost,
+      isLoading: false,
+    });
+  }
 
-        // update cart
-        await updateCart(product._id, variant._id, parseInt(this.state.qty));
+  async addToCart() {
+    // set product
+    const product = this.props.route.params.product;
 
-        // get updated items in cart
-        await this.props.getItems();
+    // set variant
+    const variant = this.props.route.params.variant;
 
-        // open add to cart modal
-        this.setState({ isOpen: true });
-    }
+    // update cart
+    await updateCart(product._id, variant._id, parseInt(this.state.qty));
 
-    render() {
+    // get updated items in cart
+    await this.props.getItems();
 
-        const {
-            description,
-        } = this.props.route.params.product;
+    // open add to cart modal
+    this.setState({isOpen: true});
+  }
 
-        const {
-            image
-        } = this.props.route.params.variant;
+  render() {
+    const {description} = this.props.route.params.product;
 
-        const {
-            qty,
-            isOpen,
-            price,
-            isLoading
-        } = this.state;
+    const {image} = this.props.route.params.variant;
 
-        const name = `${capitalize(this.props.route.params.product.name)} - ${this.props.route.params.variant.name}`;
+    const {qty, isOpen, price, isLoading} = this.state;
 
-        return (
-            <SafeAreaView style={{
-                flex: 1,
-                width: "100%",
+    const name = `${capitalize(this.props.route.params.product.name)} - ${
+      this.props.route.params.variant.name
+    }`;
+
+    return (
+      <SafeAreaView
+        style={{
+          flex: 1,
+          width: '100%',
+        }}>
+        {/* loading indicator */}
+        <LoadingIndicator loading={isLoading} />
+
+        {/* add to cart modal */}
+        <AddToCart
+          isOpen={isOpen}
+          onViewCart={() => this.props.navigation.navigate('Cart')}
+          close={() => this.setState({isOpen: false})}
+          product={{name, image, description}}
+          qty={qty}
+        />
+
+        {/* product image */}
+        <ImageBackground
+          source={{uri: image}}
+          style={{width: '100%', height: 200}}>
+          <View
+            style={{
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              justifyContent: 'center',
+              alignItems: 'center',
+              padding: units.unit5,
             }}>
-                {/* loading indicator */}
-                <LoadingIndicator
-                    loading={isLoading}
-                />
+            <View style={{backgroundColor: '#fff', padding: units.unit5}}>
+              <Paragraph style={{fontWeight: 'bold', fontSize: fonts.h3}}>
+                {capitalize(name)}
+              </Paragraph>
+            </View>
+          </View>
+        </ImageBackground>
 
-                {/* add to cart modal */}
-                <AddToCart
-                    isOpen={isOpen}
-                    onViewCart={() => this.props.navigation.navigate('Cart')}
-                    close={() => this.setState({ isOpen: false })}
-                    product={{ name, image, description }}
-                    qty={qty}
-                />
-
-                {/* product image */}
-                <ImageBackground
-                    source={{ uri: image }}
-                    style={{ width: '100%', height: 200 }}
-                >
-                    <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, justifyContent: 'center', alignItems: 'center', padding: units.unit5 }}>
-                        <View style={{ backgroundColor: '#fff', padding: units.unit5 }}>
-                            <Paragraph style={{ fontWeight: 'bold', fontSize: units.unit6 }}>{capitalize(name)}</Paragraph>
-                        </View>
-                    </View>
-                </ImageBackground>
-
-                {/* product info */}
-                <View style={{ padding: units.unit5 }}>
-                    <View style={{ backgroundColor: '#fff', padding: units.unit5, borderRadius: 5 }}>
-                        <View style={{ marginBottom: units.unit5 }}>
-                            <Paragraph style={{ fontWeight: 'bold' }}>Price</Paragraph>
-                            <Paragraph>${delimit(price.toFixed(2))} / each</Paragraph>
-                        </View>
-                        <View style={{ marginBottom: units.unit5 }}>
-                            <Paragraph style={{ fontWeight: 'bold' }}>Description</Paragraph>
-                            <Paragraph>{description}</Paragraph>
-                        </View>
-                        <View>
-                            <Paragraph style={{ fontWeight: 'bold' }}>Qty</Paragraph>
-                            <Input
-                                type="numeric"
-                                onChange={(value) => this.setState({ qty: value })}
-                                value={qty}
-                                placeholder="qty"
-                            />
-                        </View>
-                    </View>
-                    <View>
-                        <Button
-                            text="Add to Cart"
-                            onPress={() => this.addToCart()}
-                            variant="primary"
-                            disabled={!qty}
-                        />
-                    </View>
-                </View>
-            </SafeAreaView>
-        )
-    }
+        {/* product info */}
+        <View style={{padding: units.unit5}}>
+          <View
+            style={{
+              backgroundColor: '#fff',
+              padding: units.unit5,
+              borderRadius: 5,
+            }}>
+            <View style={{marginBottom: units.unit5}}>
+              <Paragraph style={{fontWeight: 'bold'}}>Price</Paragraph>
+              <Paragraph>${delimit(price.toFixed(2))} / each</Paragraph>
+            </View>
+            <View style={{marginBottom: units.unit5}}>
+              <Paragraph style={{fontWeight: 'bold'}}>Description</Paragraph>
+              <Paragraph>{description}</Paragraph>
+            </View>
+            <View>
+              <Paragraph style={{fontWeight: 'bold'}}>Qty</Paragraph>
+              <Input
+                type="numeric"
+                onChange={value => this.setState({qty: value})}
+                value={qty}
+                placeholder="qty"
+              />
+            </View>
+          </View>
+          <View>
+            <Button
+              text="Add to Cart"
+              onPress={() => this.addToCart()}
+              variant="primary"
+              disabled={!qty}
+            />
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        items: state.items,
-        irrigation: state.irrigation
-    }
+  return {
+    items: state.items,
+    irrigation: state.irrigation,
+  };
 }
 
 function mapDispatchToProps(dispatch) {
-    return bindActionCreators({
-        getItems,
-        getRules,
-        getIrrigation
-    }, dispatch)
+  return bindActionCreators(
+    {
+      getItems,
+      getRules,
+      getIrrigation,
+    },
+    dispatch,
+  );
 }
-
 
 Product = connect(mapStateToProps, mapDispatchToProps)(Product);
 
