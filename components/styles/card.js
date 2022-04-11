7aec6f8d44a78387d3127bd2bfe1ef854146fc1e
Charles Howard <charleshoward@Charless-MacBook-Pro.local>
2022-04-10 17:03:07 -0700

    work on the quotes page

---
+++ components/styles/card.js
@@ -0,0 +1,17 @@
+import ratio from '../styles/ratio';
+import units from '../styles/units';
+import colors from './colors';
+
+const card = {
+  backgroundColor: colors.white75,
+  shadowColor: colors.greenC10,
+  shadowOffset: {
+    width: 0,
+    height: 8,
+  },
+  shadowOpacity: 1,
+  shadowRadius: 8,
+  borderRadius: units.unit3,
+};
+
+export default card;
