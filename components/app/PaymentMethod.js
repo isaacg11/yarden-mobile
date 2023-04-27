8e9efb76fea033c85f5bc8a802334e60a0a2c8a3
Isaac Grey <isaacgrey@Isaacs-MBP.lan>
2023-04-26 19:57:15 -0700

    added notification management

--- components/app/PaymentMethod.js
+++ components/app/PaymentMethod.js
@@ -1,11 +1,15 @@
+// libraries
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// UI components
 import Link from '../UI/Link';
-import Button from '../../components/UI/Button';
-import Paragraph from '../../components/UI/Paragraph';
 import CreditCard from './CreditCard';
-import units from '../../components/styles/units';
+import Paragraph from '../UI/Paragraph';
+
+// styles
 import fonts from '../styles/fonts';
 import colors from '../styles/colors';
 
@@ -19,6 +23,7 @@ class PaymentMethod extends Component {
 
     return (
       <View>
+
         {/* customer card */}
         <CreditCard
           newCard={!user.payment_info}
@@ -27,6 +32,19 @@ class PaymentMethod extends Component {
         />
 
         {/* payment method start */}
+        <View style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Paragraph style={{ fontWeight: 'bold' }}>Payment Method</Paragraph>
+          {user.payment_info && (
+            <TouchableOpacity
+              onPress={() => this.setState({ isOpen: true })}>
+              <Ionicons
+                name="pencil-outline"
+                size={fonts.h3}
+                color={colors.purpleB}
+              />
+            </TouchableOpacity>
+          )}
+        </View>
         <View>
           <View>
             {!user.payment_info && (
@@ -37,13 +55,13 @@ class PaymentMethod extends Component {
                   alignItems: 'center',
                   justifyContent: 'space-between',
                 }}>
-                  <Text style={{color: colors.greenD75, fontSize: fonts.h3}}>
-                    No payment method found
-                  </Text>
-                  <Link
-                    text="Add Card"
-                    onPress={() => this.setState({isOpen: true})}
-                  />
+                <Text style={{ color: colors.greenD75, fontSize: fonts.h3 }}>
+                  No payment method found
+                </Text>
+                <Link
+                  text="Add Card"
+                  onPress={() => this.setState({ isOpen: true })}
+                />
               </View>
             )}
             {user.payment_info && (
@@ -68,10 +86,6 @@ class PaymentMethod extends Component {
                     {user.payment_info.card_exp_year})
                   </Text>
                 </Text>
-                <Link
-                  text="Edit"
-                  onPress={() => this.setState({ isOpen: true })}
-                />
               </View>
             )}
           </View>
