5333b20b0e97dc987ec52740550896d41f3077d1
Isaac Grey <isaacgrey@Isaacs-MBP-2.lan>
2024-04-01 17:36:54 -0700

    verson 2.1.59 (build 4)

--- config/testflight.js
+++ config/testflight.js
@@ -1,3 +1,3 @@
 // This will configure API calls to run against staging when app is in TestFlight
 // !!! IMPORTANT: Only use in staging, change back to FALSE before deploying to production !!!
-export default TEST_FLIGHT = true;
+export default TEST_FLIGHT = false;
